475052c41eb838b0d525bd24865ad5d20b7b2294
Sumanjit38 <sumanjit38@gmail.com>
2023-03-30 12:31:29 +0530

    Requests & Responses

--- server.js
+++ server.js
@@ -1,7 +1,50 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            //path += 'about.html';
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;    
+        default :
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    //set an html file
+    fs.readFile(path, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        }else {
+            //res.write(data);
+            
+            res.end(data);
+        }
+    })
+
+    //res.write('<head><link rel ="styleseet" href="#"></head>');
+    //res.write('<p>Hello, Kuity</p>');
+    //res.write('<p>Hello again, Kuity</p>');
+    //res.end();
 });
 
 server.listen(3000, 'localhost', () => {
